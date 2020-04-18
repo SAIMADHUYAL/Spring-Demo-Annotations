2465cc1e9c561dc9783550d39991a069cd7983a8
Saimadhu Yelakkayala <saimadhu.yal@gmail.com>
2020-04-18 13:51:40 -0500

    scope commit

--- Spring-demo-annotations/src/com/saimadhu/springAnnotationsDemo/AnnotationsDemoApp.java
+++ Spring-demo-annotations/src/com/saimadhu/springAnnotationsDemo/AnnotationsDemoApp.java
@@ -13,13 +13,16 @@ public class AnnotationsDemoApp {
 		
 		//Get the beans from the config file
 		
-		Coach theCoach = context.getBean("beanLifeCycleAnnotationMethods",Coach.class);
+		Coach beanLifeCycle = context.getBean("beanLifeCycleAnnotationMethods",Coach.class);
+		Coach theCoach = context.getBean("tennisCoach",Coach.class);
 		
 		//Call the methods
 		
+		System.out.println(beanLifeCycle.getDailyWorkout());
 		System.out.println(theCoach.getDailyWorkout());
 		
 		//call method to get daily fortune
+		System.out.println(beanLifeCycle.getDailyFortune());
 		System.out.println(theCoach.getDailyFortune());
 		
 		//Close the context
--- Spring-demo-annotations/src/com/saimadhu/springAnnotationsDemo/BeanLifeCycleAnnotationMethods.java
+++ Spring-demo-annotations/src/com/saimadhu/springAnnotationsDemo/BeanLifeCycleAnnotationMethods.java
@@ -5,10 +5,12 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@Scope("prototype")
 public class BeanLifeCycleAnnotationMethods implements Coach {
 
 	//Define Field Injection 
